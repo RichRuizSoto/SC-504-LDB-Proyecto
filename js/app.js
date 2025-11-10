60a91a6913d6d44a44dac7cfa851cf8b1a7539a4
Richard <richardruizs1998@gmail.com>
2025-11-09 23:40:52 -0600

    Mejora en rutas app.js

--- js/app.js
+++ js/app.js
@@ -1,45 +1,31 @@
 (function () {
   const $ = (s, r = document) => r.querySelector(s);
 
+  const safeView = (viewName, fallbackText) =>
+    typeof viewName === "function"
+      ? viewName
+      : () => document.createTextNode(`${fallbackText} no definida`);
+
   const routes = {
-    "#/login":
-      typeof LoginView === "function"
-        ? LoginView
-        : () => document.createTextNode("LoginView no definida"),
-
-    "#/register":
-      typeof RegisterView === "function"
-        ? RegisterView
-        : () => document.createTextNode("RegisterView no definida"),
-
-    "#/productos":
-      typeof ProductosView === "function"
-        ? ProductosView
-        : () => document.createTextNode("ProductosView no definida"),
-
-    "#/empresas":
-      typeof EmpresasView === "function"
-        ? EmpresasView
-        : () => document.createTextNode("EmpresasView no definida"),
-
-    "#/info-empresa":
-      typeof EmpresasInfoView === "function"
-        ? EmpresasInfoView
-        : () => document.createTextNode("EmpresasInfoView no definida"),
+    "#/login": safeView(LoginView, "LoginView"),
+    "#/register": safeView(RegisterView, "RegisterView"),
+    // "#/productos": safeView(ProductosView, "ProductosView"),
+    "#/empresas": safeView(EmpresasView, "EmpresasView"),
+    "#/info-empresa": safeView(EmpresasInfoView, "EmpresasInfoView"),
   };
 
   async function render(viewFn) {
     const root = document.getElementById("view-root");
     while (root.firstChild) root.removeChild(root.firstChild);
 
-    const view = await viewFn(); 
+    const view = await viewFn();
     root.appendChild(view);
   }
 
   async function router() {
     const hash = location.hash || "#/login";
     const viewFn = routes[hash] || routes["#/login"];
-    await render(viewFn); 
+    await render(viewFn);
   }
 
   window.addEventListener("hashchange", router);
